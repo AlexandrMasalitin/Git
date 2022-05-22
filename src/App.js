b9657e418e22e454509fae9d3a22462309f68f32
AlexandrMasalitin <masalitindev@gmail.com>
2022-05-22 15:41:02 +0300

    First change file in React

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Start training React + Git
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React + Git
         </a>
       </header>
     </div>
