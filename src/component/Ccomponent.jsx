c8a5e59be23befb4ebc5b5b5ce5e3d7888dbc8b5
AlexandrMasalitin <masalitindev@gmail.com>
2022-05-22 16:06:48 +0300

    Add React component.jsx

---
+++ src/component/Ccomponent.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Ccomponent extends Component {
+constructor(props) {
+  super(props)
+
+  this.state = {
+     first
+  }
+}
+
+
+  render() {
+    return (
+      <div>Ccomponent</div>
+    )
+  }
+}
---
+++ src/component/Fcomponent.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Fcomponent extends Component {
+constructor(props) {
+  super(props)
+
+  this.state = {
+     first
+  }
+}
+
+
+  render() {
+    return (
+      <div>Fcomponent</div>
+    )
+  }
+}
